97a064ad76518970c06b81f6482c4d2e10a1aa4b
Owbird <owbirdphorccuo@gmail.com>
2025-10-12 04:30:37 +0000

    chore (database): Set ttl on cache

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,9 +3,11 @@ package database
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -31,22 +33,21 @@ func GetInstance() (*Database, error) {
 		CREATE TABLE IF NOT EXISTS cache (
 			key TEXT PRIMARY KEY NOT NULL,
 			data JSONB NOT NULL,
-			updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+			updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			ttl INTEGER NOT NULL DEFAULT 1
 		)
 	`
 
 	_, err = instance.Exec(stmt)
 	if err != nil {
 		return nil, err
-
 	}
 
 	return &Database{DB: instance}, nil
 }
 
-func (db *Database) SetCache(key string, data any) error {
-
-	stmt, err := db.Prepare("INSERT OR REPLACE INTO cache(key, data) VALUES(?, ?)")
+func (db *Database) SetCache(key string, data any, ttl int64) error {
+	stmt, err := db.Prepare("INSERT OR REPLACE INTO cache(key, data, ttl) VALUES(?, ?, ?)")
 	if err != nil {
 		log.Println(err)
 		return err
@@ -60,18 +61,16 @@ func (db *Database) SetCache(key string, data any) error {
 		return err
 	}
 
-	if _, err = stmt.Exec(key, string(json)); err != nil {
+	if _, err = stmt.Exec(key, string(json), ttl); err != nil {
 		log.Println(err)
 		return err
 	}
 
 	return nil
-
 }
 
 func (db *Database) ReadCache(key string, data interface{}) error {
-
-	stmt, err := db.Prepare("select data from cache where key = ?")
+	stmt, err := db.Prepare("select data, ttl, updatedAt from cache where key = ?")
 	if err != nil {
 		log.Println(err)
 		return err
@@ -79,15 +78,30 @@ func (db *Database) ReadCache(key string, data interface{}) error {
 
 	defer stmt.Close()
 
-	var res string
-	err = stmt.QueryRow(key).Scan(&res)
+	var (
+		res       string
+		ttl       int64
+		updatedAt string
+	)
+	err = stmt.QueryRow(key).Scan(&res, &ttl, &updatedAt)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
 
+	// ttl == 0 does not expire
+	if ttl != 0 {
+		now := time.Now()
+		parsedUpdatedAt, _ := time.Parse(time.RFC3339, updatedAt)
+
+		expirationDuration := time.Duration(ttl) * 24 * time.Hour
+		if now.Sub(parsedUpdatedAt) > expirationDuration {
+			log.Printf("Data is stale for %v", key)
+			return fmt.Errorf("data is stale")
+		}
+	}
+
 	json.Unmarshal([]byte(res), &data)
 
 	return nil
-
 }
